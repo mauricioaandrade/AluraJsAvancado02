47917ba0c698cb1f3ddfe6af43fe40433d225c0e
mauricioaandrade <mauricioandrade_30@hotmail.com>
2017-11-20 20:57:22 -0300

    automacao de atualizacao da view

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -8,13 +8,13 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        this._listaNegociacoes = new ListaNegociacoes();
+        this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
+        // this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem = new Mensagem();
         this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
+        // this._mensagemView.update(this._mensagem);
     }
     _limpaFormulario() {
         this._inputData.value = '';
@@ -62,11 +62,20 @@ class NegociacaoController {
         event.preventDefault();
 
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
+        // this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem.texto = 'Negociacao adicionada com sucesso';
         this._mensagemView.update(this._mensagem);
         
         this._limpaFormulario();
     }
-}
+
+    apaga() {
+        
+            this._listaNegociacoes.esvazia();
+            // this._negociacoesView.update(this._listaNegociacoes);
+        
+            this._mensagem.texto = 'Negociações apagadas com sucesso';
+            this._mensagemView.update(this._mensagem);
+    }
+}   
--- aluraframe/client/js/app/models/ListaNegociacoes.js
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,14 +1,23 @@
 class ListaNegociacoes {
     
-        constructor() {
-            this._negociacoes = []
+        constructor(armadilha) {
+            this._negociacoes = [];
+            this._armadilha = armadilha; 
+            // this._contexto = contexto;
         }
     
         adiciona(negociacao) {
             this._negociacoes.push(negociacao);
+            this._armadilha(this);
+            // Reflect.apply(this._armadilha, this._contexto, [this]);
         }
         get negociacoes(){
             //a utilizacao do concat permite retornar uma copia da lista, tornando a original imutavel por metodos de array como .push ou .length
             return [].concat(this._negociacoes);
+        }
+        esvazia()   {
+            this._negociacoes = [];
+            this._armadilha(this);
+            // Reflect.apply(this._armadilha, this._contexto, [this]);
         }
     }
